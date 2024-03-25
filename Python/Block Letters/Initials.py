fe4e247bfb37a577847bc009289bb30212309eaa
rasulhayat <rasul.hayat.90@gmail.com>
2024-03-25 21:47:58 +0800

    python_block letters

---
+++ Python/Block Letters/Initials.py
@@ -0,0 +1,11 @@
+#Program to write initials using ASCII art, BLOCK LETTERS
+initials= '''
+RRRR      A     H   H
+R   R    A A    H   H
+R   R   A   A   H   H
+RRRR    AAAAA   HHHHH
+R R     A   A   H   H
+R  R    A   A   H   H
+R   R   A   A   H   H
+'''
+print(initials)
